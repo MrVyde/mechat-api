5b00d83e217bb49c5cb838808a1fa8ed196793f4
MrVyde <Mrvyde@outlook.com>
2026-07-22 16:59:43 +0100

    feat: configure authenticated socket client

---
+++ src/lib/lib.socket.ts
@@ -0,0 +1,9 @@
+import { io } from "socket.io-client";
+
+export const socket = io("http://localhost:5000", {
+  auth: {
+    token: typeof window !== "undefined"
+      ? localStorage.getItem("token")
+      : null,
+  },
+});
