64a641005037e01c38d51c580a1473f38e7c8645
MrVyde <Mrvyde@outlook.com>
2026-07-22 16:57:00 +0100

    feat: create register, login, and user session endpoints

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import * as authController from "../controllers/auth.controller";
+import { registerValidator, loginValidator } from "../middlewares/auth.validator";
+import { validate } from "../middlewares/validate.middleware";
+import { authMiddleware } from "../middlewares/auth.middleware";
+
+const router = Router();
+
+router.post("/register",registerValidator,validate,authController.register);
+
+router.post("/login",loginValidator,validate,authController.login);
+
+router.get("/me", authMiddleware, authController.me);
+
+router.post("/logout", authController.logout);
+
+export default router;
